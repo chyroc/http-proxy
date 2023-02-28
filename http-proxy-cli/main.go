dab19c96cbd41e75e45d355d4555dde7d7d8e5e5
chyroc <chyroc@qq.com>
2023-02-28 13:07:48 +0800

    feat: add cli

---
+++ http-proxy-cli/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/chyroc/http-proxy"
+)
+
+func newConfig(usernameConfig, passwordConfig string) *http_proxy.Config {
+	if usernameConfig == "" && passwordConfig == "" {
+		return &http_proxy.Config{}
+	}
+
+	return &http_proxy.Config{
+		AuthChecker: func(username, password string) error {
+			if username == usernameConfig && password == passwordConfig {
+				return nil
+			}
+			return fmt.Errorf("auth failed")
+		},
+	}
+}
+
+func main() {
+	usernameConfig := os.Getenv("HTTP_PROXY_USERNAME")
+	passwordConfig := os.Getenv("HTTP_PROXY_PASSWORD")
+	serverHost := os.Getenv("HTTP_PROXY_SERVER_HOST")
+	if serverHost == "" {
+		serverHost = "127.0.0.1:8080"
+	}
+	proxy := http_proxy.NewHTTPProxy(newConfig(usernameConfig, passwordConfig))
+
+	if err := http.ListenAndServe(serverHost, proxy); err != nil {
+		log.Fatalln(err)
+	}
+}
